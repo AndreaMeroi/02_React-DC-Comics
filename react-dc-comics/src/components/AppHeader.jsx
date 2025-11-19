080f4ef49714bcacf230c3d4ef358211cb2e348c
AndreaMeroi <andreameroidublin@gmail.com>
2025-11-19 15:07:42 +0100

    add 2 step ex - better body fit to be done

--- react-dc-comics/src/components/AppHeader.jsx
+++ react-dc-comics/src/components/AppHeader.jsx
@@ -4,19 +4,26 @@ export default function AppHeader() {
 
     return (
         <header>
-            <img src={headerImage} alt="" />
-            <nav>
-                <a href="">Characters</a>
-                <a href="">comics</a>
-                <a href="">movies</a>
-                <a href="">tv</a>
-                <a href="">games</a>
-                <a href="">collectibles</a>
-                <a href="">videos</a>
-                <a href="">fans</a>
-                <a href="">news</a>
-                <a href="">shop</a>
-            </nav>
+            <div className='headerContainer'>
+
+                <  img src={headerImage} alt="" />
+                <nav>
+                    <a href="">Characters</a>
+                    <a href="">comics</a>
+                    <a href="">movies</a>
+                    <a href="">tv</a>
+                    <a href="">games</a>
+                    <a href="">collectibles</a>
+                    <a href="">videos</a>
+                    <a href="">fans</a>
+                    <a href="">news</a>
+                    <a href="">shop</a>
+                </nav>
+
+            </div>
+
+
+
         </header>
 
     )
--- react-dc-comics/src/components/AppMain.jsx
+++ react-dc-comics/src/components/AppMain.jsx
@@ -8,31 +8,41 @@ export default function AppMain() {
     return (
 
         <main>
-            <h3> Content goes Here  </h3>
-            <nav>
-                <ul>
-                    <li>
-                        <img src={DcImage} alt="" />
-                        <a href="">digital comics</a>
-                    </li>
-                    <li>
-                        <img src={DcMerch} alt="" />
-                        <a href="">digital merchandise</a>
-                    </li>
-                    <li>
-                        <img src={DcSubs} alt="" />
-                        <a href="">subscription</a>
-                    </li>
-                    <li>
-                        <img src={DcShopLoc} alt="" />
-                        <a href="">comic shop locator</a>
-                    </li>
-                    <li>
-                        <img src={DcPowerVisa} alt="" />
-                        <a href="">dc power visa</a>
-                    </li>
-                </ul>
-            </nav>
+
+            <div className='mainContainer'>
+
+
+                <h3>Content goes Here</h3>
+
+                <nav >
+                    <ul>
+                        <li>
+                            <img src={DcImage} alt="" />
+                            <a href="">digital comics</a>
+                        </li>
+                        <li>
+                            <img src={DcMerch} alt="" />
+                            <a href="">digital merchandise</a>
+                        </li>
+                        <li>
+                            <img src={DcSubs} alt="" />
+                            <a href="">subscription</a>
+                        </li>
+                        <li>
+                            <img src={DcShopLoc} alt="" />
+                            <a href="">comic shop locator</a>
+                        </li>
+                        <li>
+                            <img src={DcPowerVisa} alt="" />
+                            <a href="">dc power visa</a>
+                        </li>
+                    </ul>
+                </nav>
+
+            </div>
+
+
+
         </main>
     )
 }
